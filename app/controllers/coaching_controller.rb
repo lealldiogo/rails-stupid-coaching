b9b3cfedd3ca2aaec006767901b9f2a9e46d4c09
Diogo Loreto Leal <lealldiogo@gmail.com>
2017-02-13 12:16:34 -0200

    First Rails App

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,16 @@
+class CoachingController < ApplicationController
+  def answer
+    @question = params[:query]
+    if @question == "I am going to work right now!"
+      @answer = ""
+    elsif @question.end_with? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+
+  end
+end
